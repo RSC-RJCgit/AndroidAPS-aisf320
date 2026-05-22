e369e2ab1a16411afdb79a795b1180e6ecdbe7a3
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-05-23 00:53:10 +0930

    CAP081

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 object Versions {
 
     const
-    val appVersion = "3.4.0.0+ai320 CAP081" +
+    val appVersion= "3.4.0.0+ai320 CAP081" +
         ""
     const val versionCode = 1500
 
