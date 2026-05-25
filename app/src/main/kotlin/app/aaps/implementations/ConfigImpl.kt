acfee225a119f5f781a9cfd508879c1b5573feb8
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-05-26 01:07:19 +0930

    320.001

--- app/src/main/kotlin/app/aaps/implementations/ConfigImpl.kt
+++ app/src/main/kotlin/app/aaps/implementations/ConfigImpl.kt
@@ -45,6 +45,7 @@ class ConfigImpl @Inject constructor(
     private var doNotSendSmsOnProfileChange: Boolean? = null
     private var enableAutotune: Boolean? = null
     private var disableLeakCanary: Boolean? = null
+    private var enableOmnipodDriftCompensation: Boolean? = null
 
     override fun isEngineeringModeOrRelease(): Boolean = if (!APS) true else isEngineeringMode() || !isDev()
     override fun isEngineeringMode(): Boolean = true        //isEngineeringMode ?: (fileListProvider.get().ensureExtraDirExists()?.findFile("engineering_mode") != null).also { isEngineeringMode = it }
@@ -55,4 +56,5 @@ class ConfigImpl @Inject constructor(
     override fun doNotSendSmsOnProfileChange(): Boolean = doNotSendSmsOnProfileChange ?: (fileListProvider.get().ensureExtraDirExists()?.findFile("do_not_send_sms_on_profile_change") != null).also { doNotSendSmsOnProfileChange = it }
     override fun enableAutotune(): Boolean = enableAutotune ?: (fileListProvider.get().ensureExtraDirExists()?.findFile("enable_autotune") != null).also { enableAutotune = it }
     override fun disableLeakCanary(): Boolean = disableLeakCanary ?: (fileListProvider.get().ensureExtraDirExists()?.findFile("disable_leakcanary") != null).also { disableLeakCanary = it }
+    override fun enableOmnipodDriftCompensation(): Boolean = enableOmnipodDriftCompensation ?: (fileListProvider.get().ensureExtraDirExists()?.findFile("omnipod_drift_compensation") != null).also { enableOmnipodDriftCompensation = it }
 }
--- core/interfaces/src/main/kotlin/app/aaps/core/interfaces/configuration/Config.kt
+++ core/interfaces/src/main/kotlin/app/aaps/core/interfaces/configuration/Config.kt
@@ -39,4 +39,6 @@ interface Config {
      * Disable LeakCanary (memory leaks detection). By default it's enabled in DEBUG builds.
      */
     fun disableLeakCanary(): Boolean
+
+    fun enableOmnipodDriftCompensation(): Boolean
 }
