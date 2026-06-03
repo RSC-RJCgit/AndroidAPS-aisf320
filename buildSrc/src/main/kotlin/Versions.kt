005fe5493ccaa760638c674345245370e175b1d2
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-06-03 15:47:38 +0930

    320.TDD024NS4

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 @Suppress("ConstPropertyName")
 object Versions {
 
-    const val appVersion = "3.4.2.1+a320.TDD024NS3"
+    const val appVersion = "3.4.2.1+a320.TDD024NS4"
     const val versionCode = 1500
 
     const val compileSdk = 36
