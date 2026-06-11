2b8e13b75414fcf92f28e313bbf5d288e89bc5aa
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-06-11 12:27:43 +0930

    320TZ000

--- shared/tests/src/main/kotlin/app/aaps/shared/tests/HardLimitsMock.kt
+++ shared/tests/src/main/kotlin/app/aaps/shared/tests/HardLimitsMock.kt
@@ -25,16 +25,16 @@ class HardLimitsMock @Inject constructor(
 
         // Very Hard Limits Ranges
         // First value is the Lowest and second value is the Highest a Limit can define
-        val VERY_HARD_LIMIT_MIN_BG = doubleArrayOf(80.0, 180.0)
-        val VERY_HARD_LIMIT_MAX_BG = doubleArrayOf(90.0, 200.0)
-        val VERY_HARD_LIMIT_TARGET_BG = doubleArrayOf(80.0, 200.0)
+        val VERY_HARD_LIMIT_MIN_BG = doubleArrayOf(72.0, 180.0)
+        val VERY_HARD_LIMIT_MAX_BG = doubleArrayOf(72.0, 200.0)
+        val VERY_HARD_LIMIT_TARGET_BG = doubleArrayOf(72.0, 200.0)
 
         // Very Hard Limits Ranges for Temp Targets
         val VERY_HARD_LIMIT_TEMP_MIN_BG = intArrayOf(72, 180)
         val VERY_HARD_LIMIT_TEMP_MAX_BG = intArrayOf(72, 270)
         val VERY_HARD_LIMIT_TEMP_TARGET_BG = intArrayOf(72, 200)
         val MIN_DIA = doubleArrayOf(5.0, 5.0, 5.0, 5.0, 5.0)
-        val MAX_DIA = doubleArrayOf(9.0, 9.0, 9.0, 9.0, 10.0)
+        val MAX_DIA = doubleArrayOf(10.0, 10.0, 10.0, 10.0, 10.0)
         val MIN_IC = doubleArrayOf(2.0, 2.0, 2.0, 2.0, 0.3)
         val MAX_IC = doubleArrayOf(100.0, 100.0, 100.0, 100.0, 100.0)
         const val MIN_ISF = 2.0 // mgdl
