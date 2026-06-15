e5a376e5e56cce4bba762b68f7f50858a128018e
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-06-15 12:19:40 +0930

    RJC000

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 object Versions {
 
     // On change edit aaps-ci.yml
-    const val appVersion = "4.0.0-dev-a-RJC000"
+    const val appVersion = "4.0.0-dev-RJC000-rc"
     const val versionCode = 1500
 
     const val compileSdk = 36
