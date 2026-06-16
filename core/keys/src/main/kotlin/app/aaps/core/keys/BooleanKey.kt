e779c1357805ca8da32f55ebedc683aec53de947
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-06-16 23:28:56 +0930

    C100-rc

--- core/keys/src/main/kotlin/app/aaps/core/keys/BooleanKey.kt
+++ core/keys/src/main/kotlin/app/aaps/core/keys/BooleanKey.kt
@@ -118,6 +118,8 @@ enum class BooleanKey(
     SmsAllowRemoteCommands("smscommunicator_remotecommandsallowed", false, R.string.pref_title_sms_allow_remote_commands),
     SmsReportPumpUnreachable("smscommunicator_report_pump_unreachable", true, R.string.pref_title_sms_report_pump_unreachable, R.string.pref_summary_sms_report_pump_unreachable),
 
+    AutomationStatesEnabled("automation_states_enabled", false, R.string.pref_title_automation_states_enabled),
+
     VirtualPumpStatusUpload("virtualpump_uploadstatus", false, R.string.pref_title_virtual_pump_status_upload, showInNsClientMode = false),
     NsClientUploadData("ns_upload", true, R.string.pref_title_ns_upload_data, R.string.pref_summary_ns_upload_data, showInNsClientMode = false, hideParentScreenIfHidden = true),
     NsClientAcceptCgmData("ns_receive_cgm", false, R.string.pref_title_ns_receive_cgm, R.string.pref_summary_ns_receive_cgm, showInNsClientMode = false, hideParentScreenIfHidden = true),
