5d58ed0739fb234667f26606a06e14b8d69bed17
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-06-17 07:57:35 +0930

    RJC102-rc

--- plugins/automation-state/src/main/kotlin/app/aaps/plugins/automationstate/di/AutomationStateModule.kt
+++ plugins/automation-state/src/main/kotlin/app/aaps/plugins/automationstate/di/AutomationStateModule.kt
@@ -1,14 +1,9 @@
 ﻿package app.aaps.plugins.automationstate.di
 
 import app.aaps.core.interfaces.automation.AutomationStateInterface
-import app.aaps.plugins.automationstate.dialogs.AutomationAddStateDialog
-import app.aaps.plugins.automationstate.dialogs.AutomationStateValuesDialog
 import app.aaps.plugins.automationstate.services.AutomationStateService
-import app.aaps.plugins.automationstate.ui.AutomationStateFragment
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
-import dagger.android.ContributesAndroidInjector
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -16,16 +11,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 @Suppress("unused")
-abstract class AutomationStateModule {
-
-    @ContributesAndroidInjector
-    abstract fun contributesAutomationStateFragment(): AutomationStateFragment
-
-    @ContributesAndroidInjector
-    abstract fun contributesAutomationAddStateDialog(): AutomationAddStateDialog
-
-    @ContributesAndroidInjector
-    abstract fun contributesAutomationStateValuesDialog(): AutomationStateValuesDialog
+class AutomationStateModule {
 
     companion object {
         @Provides
