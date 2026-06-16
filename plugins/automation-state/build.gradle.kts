a6b32930db466f722afce3e3060c3b5b3f430dcf
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-06-17 08:30:53 +0930

    RJC102-rc

--- plugins/automation-state/build.gradle.kts
+++ plugins/automation-state/build.gradle.kts
@@ -1,6 +1,7 @@
-plugins {
+﻿plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
     id("android-module-dependencies")
     id("test-module-dependencies")
     id("jacoco-module-dependencies")
@@ -19,15 +20,17 @@ dependencies {
     implementation(project(":core:ui"))
     implementation(project(":plugins:automation"))
 
+    implementation(libs.com.google.dagger.hilt.android)
+
     testImplementation(project(":shared:tests"))
     testImplementation(project(":shared:impl"))
     testImplementation(project(":implementation"))
     testImplementation(project(":plugins:main"))
     testImplementation(project(":pump:virtual"))
 
-    api(libs.androidx.constraintlayout)
     api(libs.kotlin.reflect)
 
     ksp(libs.com.google.dagger.compiler)
+    ksp(libs.com.google.dagger.hilt.compiler)
     ksp(libs.com.google.dagger.android.processor)
 }
