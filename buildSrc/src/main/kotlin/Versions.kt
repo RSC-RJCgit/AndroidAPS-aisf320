4352e1313bd26be3fa69cfdaecc0d0d293ef5716
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-07-07 14:38:21 +0930

    v4ISFm002

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 object Versions {
 
     // On change edit aaps-ci.yml
-    const val appVersion = "4.0.0-v4ISFm000"
+    const val appVersion = "4.0.0-v4ISFm002"
     const val versionCode = 1500
 
     const val compileSdk = 36
