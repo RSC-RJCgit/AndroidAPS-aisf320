c3b01f65b4575aded2f89ab42ab3ae127931731a
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-07-13 22:25:35 +0930

    320TDD2AU250

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 @Suppress("ConstPropertyName")
 object Versions {
 
-    const val appVersion = "3.4.2.1+320TDD2AU249"
+    const val appVersion = "3.4.2.1+320TDD2AU250"
     const val versionCode = 1500
 
     const val compileSdk = 36
