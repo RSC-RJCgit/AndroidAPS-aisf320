02d0f3ccce8353094832a97071ae208ab84fb6ea
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-07-26 21:22:24 +0930

    320TDD2AU362next

--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/DetermineBasalAutoISF.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/DetermineBasalAutoISF.kt
@@ -339,7 +339,7 @@ class DetermineBasalAutoISF @Inject constructor(
 
         if (autoIsfMode) {
             consoleError.add("----------------------------------")
-            consoleError.add("start AutoISF ${profile.autoISF_version} __ 320TDD2AU361")
+            consoleError.add("start AutoISF ${profile.autoISF_version} __ 320TDD2AU362")
             consoleError.add("----------------------------------")
             consoleError.add("Sensitivity: ${autosens_data.sensResult}")
             consoleError.addAll(auto_isf_consoleLog)
@@ -709,7 +709,7 @@ class DetermineBasalAutoISF @Inject constructor(
         val TwilightTimeMins = 0
         val TwilightTimeDec = TwilightTimeAM + TwilightTimeMins / 100
         rT.reason.append(
-            " 320TDD2AU361 COB: ${round(meal_data.mealCOB, 1).withoutZeros()}, Dev: ${convert_bg(deviation.toDouble())}, BGI: ${convert_bg(bgi)}, ISF: ${convert_isf(sens)}, CR: ${
+            " 320TDD2AU362 COB: ${round(meal_data.mealCOB, 1).withoutZeros()}, Dev: ${convert_bg(deviation.toDouble())}, BGI: ${convert_bg(bgi)}, ISF: ${convert_isf(sens)}, CR: ${
                 round(profile.carb_ratio, 2)
                     .withoutZeros()
             }, Target: ${convert_bg(target_bg)}, minPredBG ${convert_bg(minPredBG)}, minGuardBG ${convert_bg(minGuardBG)}, IOBpredBG ${convert_bg(lastIOBpredBG)}"
@@ -779,7 +779,7 @@ class DetermineBasalAutoISF @Inject constructor(
         consoleError.add("TwilightTimeDec: " + TwilightTimeDec + " ; ")
 
         //================================================================================/
-        var lastCarbAge = 361.0
+        var lastCarbAge = 362.0
         if (meal_data.carbs != null && meal_data.carbs > 0) {
             lastCarbAge = round(((systemTime - meal_data.lastCarbTime) / 60000.0), 2)
         }
@@ -1625,5 +1625,5 @@ class DetermineBasalAutoISF @Inject constructor(
 }
 
 /*
-DetermineBasalAutoISF.kt320TDD2AU361
+DetermineBasalAutoISF.kt320TDD2AU362
 */
