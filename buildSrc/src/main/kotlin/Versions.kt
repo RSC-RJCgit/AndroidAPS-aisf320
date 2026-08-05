aa852ef6cd8ffa84365f5dc1972801fcce2093c2
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-08-05 15:47:10 +0930

    aisf321_494BGsUKFcarbsClamp

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 @Suppress("ConstPropertyName")
 object Versions {
 
-    const val appVersion = "3.4.2.5+aisf321_493"
+    const val appVersion = "3.4.2.5+aisf321_494"
     const val versionCode = 1500
 
     const val compileSdk = 36
--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/OpenAPSAutoISFPlugin.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/OpenAPSAutoISFPlugin.kt
@@ -4801,5 +4801,5 @@ open class OpenAPSAutoISFPlugin @Inject constructor(
 }
 
 /*
-OpenAPSAutoISFPlugin.ktaisf321_493
+OpenAPSAutoISFPlugin.ktaisf321_494
 */
