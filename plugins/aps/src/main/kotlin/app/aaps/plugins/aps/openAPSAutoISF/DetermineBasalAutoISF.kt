20845bd660ef855656f332cde9942e103a86ef03
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-08-13 07:21:39 +0930

    aisf321_556next

--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/DetermineBasalAutoISF.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/DetermineBasalAutoISF.kt
@@ -357,7 +357,7 @@ class DetermineBasalAutoISF @Inject constructor(
 
         if (autoIsfMode) {
             consoleError.add("----------------------------------")
-            consoleError.add("start AutoISF ${profile.autoISF_version} __ aisf321_565")
+            consoleError.add("start AutoISF ${profile.autoISF_version} __ aisf321_566")
             consoleError.add("----------------------------------")
             consoleError.add("Sensitivity: ${autosens_data.sensResult}")
             consoleError.addAll(auto_isf_consoleLog)
@@ -733,7 +733,7 @@ class DetermineBasalAutoISF @Inject constructor(
         val TwilightTimeMins = 0
         val TwilightTimeDec = TwilightTimeAM + TwilightTimeMins / 100
         rT.reason.append(
-            " aisf321_565 COB: ${round(meal_data.mealCOB, 1).withoutZeros()}, Dev: ${convert_bg2(deviation.toDouble())}, BGI: ${convert_bg2(bgi)}, ISF: ${convert_isf(sens)}, CR: ${
+            " aisf321_566 COB: ${round(meal_data.mealCOB, 1).withoutZeros()}, Dev: ${convert_bg2(deviation.toDouble())}, BGI: ${convert_bg2(bgi)}, ISF: ${convert_isf(sens)}, CR: ${
                 round(profile.carb_ratio, 2)
                     .withoutZeros()
             }, Target: ${convert_bg(target_bg)}, minPredBG ${convert_bg(minPredBG)}, minGuardBG ${convert_bg(minGuardBG)}, IOBpredBG ${convert_bg(lastIOBpredBG)}"
@@ -1840,5 +1840,5 @@ class DetermineBasalAutoISF @Inject constructor(
 }
 
 /*
-DetermineBasalAutoISF.ktaisf321_565
+DetermineBasalAutoISF.ktaisf321_566
 */
--- plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/OpenAPSAutoISFPlugin.kt
+++ plugins/aps/src/main/kotlin/app/aaps/plugins/aps/openAPSAutoISF/OpenAPSAutoISFPlugin.kt
@@ -5581,5 +5581,5 @@ open class OpenAPSAutoISFPlugin @Inject constructor(
 }
 
 /*
-OpenAPSAutoISFPlugin.ktaisf321_565
+OpenAPSAutoISFPlugin.ktaisf321_566
 */
