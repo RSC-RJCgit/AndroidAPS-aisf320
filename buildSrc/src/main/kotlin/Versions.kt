b6fdcbc8640bcb424c1caa171c3bb0f23ffd7a6f
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-09-02 21:53:00 +0930

    aisf321UK_760next

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 object Versions {
 
     // On change edit aaps-ci.yml
-    const val appVersion = "3.4.2.6+aisf321UK_759"
+    const val appVersion = "3.4.2.6+aisf321UK_760"
     const val versionCode = 1500
 
     const val compileSdk = 36
