d8f7b47bd081827fde4630b59ea2407e7e2581e0
RSC-RJCgit <142757046+RSC-RJCgit@users.noreply.github.com>
2026-09-24 22:24:05 +1000

    kmp000

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
 
     // On change edit aaps-ci.yml
     // UKF3426 is this port. The -dev keeps it a development build. A name with no hyphen is treated as a release.
-    const val appVersion = "UKF3426-dev"
+    const val appVersion = "4.0.0-dev-c"
     const val versionCode = 1500
 
     const val compileSdk = 37
